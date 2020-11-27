6a4577f35edbaa916773705435fb7efcedf2297f
MiroslawLatka <MiroslawLatka@pwr.edu.pl>
2020-11-27 16:06:59 +0100

    Second commit

--- src/main/java/edu/ib/Main.java
+++ src/main/java/edu/ib/Main.java
@@ -5,6 +5,7 @@ import org.mariuszgromada.math.mxparser.Function;
 
 public class Main {
     public static void main(String[] args) {
+        //Demo update
         Function at= new Function("f(x)=x*x");
         Expression e1= new Expression("f(2)",at);
         System.out.println(e1.calculate());
